311a8c3e28ea4996733c00a7a05f55efbf6dd771
EricJ <ericjardon@hotmail.com>
2021-05-20 21:47:48 -0500

    Routes and super advanced queries

--- routers/titlesRouter.js
+++ routers/titlesRouter.js
@@ -2,25 +2,101 @@ const express = require("express");
 require("dotenv").config(); // to load the values specified in .env
 const router = express.Router();
 
-// URL Query params: type, name, country
+const titles = require('../db').db().collection('titles');
+
+// URL Query params: type, title, country
 
 router.get("/", () => {
   console.log("Get all the titles in the database");
 }); // getAll
 
-router.get("/query/", (req, res) => {
+router.get("/query/", async (req, res) => {
   const queryString = req.query;
-  console.log("Query string", queryString);
-  console.log(req.query.type);
-  console.log(req.query.name);
-  console.log(req.query.country);
-  res.send("Your query is stupid");
 
-  const QUERY = req.query;
-}); // getAll
+  const query = {};
 
-/* router.get("/:country", () => {
-  console.log("Query of the movies by a given country");
-}); */
+  if (queryString.type!==''){
+    query.type = queryString.type;
+  }
+
+  if (queryString.title!==''){
+    query.title = queryString.title;
+  }
+  
+  if (queryString.country!==''){
+    query.country = queryString.country;
+  }
+  
+  console.log("Query to send:", query);
+  let results = await titles.find(query).toArray();
+  console.log(results);
+  // res.status(200).json(result);
+});
+
+// STATISTICS
+
+router.get("/stats/", async (req, res) => {
+  const queryString = req.query;
+
+  const query = {};
+
+  if (queryString.type!==''){
+    query.type = queryString.type;
+  }
+
+  if (queryString.title!==''){
+    query.title = queryString.title;
+  }
+  
+  if (queryString.country!==''){
+    query.country = queryString.country;
+  }
+  
+  console.log("Query to send:", query);
+  let results = await titles.find(query).count();
+  console.log(results);
+});
 
 module.exports = router;
+
+// List of movies and 
+// router.get("/actor/", async (req, res) => {
+//   const actorName = req.query.cast;
+//   if (typeof(actorName) != "string") {
+//     res.status(500).send("ERROR");
+//   }
+//   console.log("Searching actor..", actorName);
+
+//   let results = await titles.aggregate({$match:{$text: {$search: `"${actorName}"`}}}).toArray();
+//   if (results.length) {
+//     res.status(200).send(results);
+//   } else {
+//     res.status(200).send("No results");
+//   }
+// });
+
+router.get("/actor/", async (req, res) => {
+    const actorName = req.query.cast;
+    if (typeof(actorName) != "string") {
+      res.status(500).send("ERROR");
+    }
+    console.log("Searching actor..", actorName);
+  
+    let results = await titles.aggregate({$match:{$text: {$search: `"${actorName}"`}}}).toArray();
+    // do not try, no text index
+    res.send("Query for actor");
+  });
+
+/* search(searchTerm) {
+        return new Promise(async (resolve, reject) => {
+            if (typeof(searchTerm) == "string"){
+                let searchResults = await Post.postQuery([
+                {$match: {$text: {$search: searchTerm}}},
+                {$sort: {score: {$meta: "textScore"}}}    // score is how relevant
+                ]);
+                resolve(searchResults);
+            }else {
+                reject();
+            }
+        })
+} */
