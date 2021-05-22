8b3499b1cdc89a5413ca29717bcb15d771427123
Bryan Monroy <a01026848@itesm.mx>
2021-05-21 21:15:57 -0500

    Connection back with front

--- db.js
+++ db.js
@@ -1,11 +1,15 @@
 // remember to install mongodb with npm
-const mongodb = require('mongodb');
-const dotenv = require('dotenv');
+const mongodb = require("mongodb");
+const dotenv = require("dotenv");
 
-dotenv.config();    // loads in all the values defined in .env
+dotenv.config(); // loads in all the values defined in .env
 
-mongodb.connect(process.env.DB_CONNECTION, {useNewUrlParser:true, useUnifiedTopology: true},function(err, client) {
-    module.exports = client;   // so we can use it from any file in the app
-    const server = require('./server');
-    server.listen(process.env.PORT, () => console.log("Bryajin"));
-});
+mongodb.connect(
+  process.env.DB_CONNECTION,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  function (err, client) {
+    module.exports = client; // so we can use it from any file in the app
+    const server = require("./server");
+    server.listen(process.env.PORT, () => console.log("Netflix DB running"));
+  }
+);
